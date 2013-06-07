f6ec24da3485a864e53cc3f26ba1b53e6d8c4ed5
Clashsoft <clash2@hotmail.de>
2013-06-07 20:54:24 +0200

    Mod File, Font Renderer
    The Clashsoft Font Renderer allows you to write in any color you want
    when you add the prefix §C[<Hexadecimal Color>] to your text.

---
+++ ClashsoftMod.java
@@ -0,0 +1,18 @@
+package clashsoft.clashsoftapi;
+
+import net.minecraft.client.Minecraft;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "ClashsoftAPI", name = "Clashsoft API", version = CSUtil.CURRENT_VERION)
+public class ClashsoftMod
+{	
+	protected static CSFontRenderer csFontRenderer = new CSFontRenderer(Minecraft.getMinecraft().gameSettings, "/font/default.png", Minecraft.getMinecraft().renderEngine, false);
+	
+	@Init
+	public void init(FMLInitializationEvent event)
+	{
+		csFontRenderer = new CSFontRenderer(Minecraft.getMinecraft().gameSettings, "/font/default.png", Minecraft.getMinecraft().renderEngine, false);
+	}
+}
