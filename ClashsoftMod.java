f9cc4d98d025e2fe64b79c40e07536fc87051534
Clashsoft <clash2@hotmail.de>
2013-07-25 00:15:42 +0200

    Fixed Mod not working on servers

--- ClashsoftMod.java
+++ ClashsoftMod.java
@@ -2,7 +2,9 @@ package clashsoft.clashsoftapi;
 
 import clashsoft.clashsoftapi.util.CSUtil;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.PostInit;
 import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
@@ -10,17 +12,22 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.network.NetworkRegistry;
 
 @Mod(modid = "ClashsoftAPI", name = "Clashsoft API", version = CSUtil.CURRENT_VERION)
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class ClashsoftMod
 {	
-	@SidedProxy(clientSide = "clashsoft.clashsoftapi.ClientProxy", serverSide = "clashsoft.clashsoftapi.CommonProxy")
+	@Instance("ClashsoftAPI")
+	public static ClashsoftMod instance;
+	
+	@SidedProxy(modId = "ClashsoftAPI", clientSide = "clashsoft.clashsoftapi.ClientProxy", serverSide = "clashsoft.clashsoftapi.CommonProxy")
 	public static CommonProxy proxy;
 	
-	@PreInit
+	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{	
+		NetworkRegistry.instance().registerGuiHandler(instance, proxy);
 		proxy.registerRenderers();
 	}
 }
