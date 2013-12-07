d9bea1f284858c535844a4dc8ecfe5a6b747fbde
Clashsoft <clash2@hotmail.de>
2013-12-07 10:36:03 +0100

    Fixed ItemCustomArmor crash

--- minecraft/item/ItemCustomArmor.java
+++ minecraft/item/ItemCustomArmor.java
@@ -2,20 +2,34 @@ package clashsoft.cslib.minecraft.item;
 
 import clashsoft.cslib.reflect.CSReflection;
 
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
 
 public class ItemCustomArmor extends ItemArmor
 {
 	public ItemCustomArmor(int itemID, EnumArmorMaterial material, int renderIndex, int armorType)
 	{
 		super(itemID, material, renderIndex, armorType & 3);
-		CSReflection.setValue(this, armorType, 4);
-		CSReflection.setValue(this, getDamageReductionAmount(material), 5);
+		CSReflection.setValue(ItemArmor.class, this, armorType, 4);
+		CSReflection.setValue(ItemArmor.class, this, getDamageReductionAmount(material), 5);
 	}
 	
-	public float getDamageReductionAmount(EnumArmorMaterial material)
+	@Override
+	public void registerIcons(IconRegister par1IconRegister)
+	{
+		this.itemIcon = par1IconRegister.registerIcon(this.getIconString());
+	}
+	
+	public int getDamageReductionAmount(EnumArmorMaterial material)
 	{
 		return material.getDamageReductionAmount(this.armorType & 3);
+	}
+	
+	public boolean isValidArmor(ItemStack stack, int armorType, Entity entity)
+	{
+		return armorType == this.armorType;
 	}
 }
