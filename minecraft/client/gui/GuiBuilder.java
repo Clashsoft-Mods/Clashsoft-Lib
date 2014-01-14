6ef9196c2961b9dd273f90090875447f60569094
Clashsoft <clash2@hotmail.de>
2014-01-14 20:57:37 +0100

    GuiBuilder

--- minecraft/client/gui/GuiBuilder.java
+++ minecraft/client/gui/GuiBuilder.java
@@ -7,13 +7,13 @@ import net.minecraft.util.ResourceLocation;
 
 public class GuiBuilder extends Gui
 {
-	public static final ResourceLocation slots = new ResourceLocation("cslib", "textures/gui/container/slots.png");
-	public static final ResourceLocation progress = new ResourceLocation("cslib", "textures/gui/container/progress.png");
-	public static final ResourceLocation window = new ResourceLocation("cslib", "textures/gui/container/window.png");
-	public static final ResourceLocation widgets = new ResourceLocation("cslib", "textures/gui/container/widgets.png");
+	public static final ResourceLocation	slots		= new ResourceLocation("cslib", "textures/gui/container/slots.png");
+	public static final ResourceLocation	progress	= new ResourceLocation("cslib", "textures/gui/container/progress.png");
+	public static final ResourceLocation	window		= new ResourceLocation("cslib", "textures/gui/container/window.png");
+	public static final ResourceLocation	widgets		= new ResourceLocation("cslib", "textures/gui/container/widgets.png");
 	
-	public final GuiScreen gui;
-	public final Minecraft mc;
+	public final GuiScreen					gui;
+	public final Minecraft					mc;
 	
 	public GuiBuilder(GuiScreen gui)
 	{
@@ -24,6 +24,69 @@ public class GuiBuilder extends Gui
 	public void bind(ResourceLocation resourceLocation)
 	{
 		this.mc.renderEngine.bindTexture(resourceLocation);
+	}
+	
+	public void drawFrame(int x, int y, int width, int height)
+	{
+		bind(window);
+		drawFrame_(x, y, width, height);
+	}
+	
+	public void drawFrame_(int x, int y, int width, int height)
+	{
+		boolean flag1 = width > 176;
+		boolean flag2 = height > 166;
+		
+		int i = width - 4;
+		int j = height - 4;
+		int k = flag1 ? 172 : i;
+		int l = flag2 ? 162 : j;
+		
+		this.drawTexturedModalRect(x, y, 0, 0, k, l);
+		this.drawTexturedModalRect(x + i, y, 172, 0, 4, l);
+		this.drawTexturedModalRect(x, y + j, 0, 162, k, 4);
+		this.drawTexturedModalRect(x + i, y + j, 172, 162, 4, 4);
+		
+		if (flag1)
+		{
+			int x1 = x + 172;
+			int k1 = i - 172;
+			this.drawTexturedModalRect(x1, y, 4, 0, k1, l);
+			this.drawTexturedModalRect(x1, y + j, 4, 162, k1, 4);
+		}
+		if (flag2)
+		{
+			int y1 = y + 162;
+			int l1 = j - 162;
+			this.drawTexturedModalRect(x, y1, 0, 4, k, l1);
+			this.drawTexturedModalRect(x + i, y1, 172, 4, 4, l1);
+			
+			if (flag1)
+			{
+				this.drawTexturedModalRect(x + 172, y1, 4, 4, i - 172, l1);
+			}
+		}
+	}
+	
+	public void drawPlayerBackgroundL(int x, int y)
+	{
+		drawWidget(x, y, 0, 0, 54, 72);
+	}
+	
+	public void drawPlayerBackgroundS(int x, int y)
+	{
+		drawWidget(x, y, 54, 0, 34, 45);
+	}
+	
+	public void drawWidget(int x, int y, int u, int v, int width, int height)
+	{
+		bind(widgets);
+		drawWidget_(x, y, u, v, width, height);
+	}
+	
+	public void drawWidget_(int x, int y, int u, int v, int width, int height)
+	{
+		this.drawTexturedModalRect(x, y, u, v, width, height);
 	}
 	
 	public void drawSlots(int[] x, int[] y)
@@ -66,11 +129,11 @@ public class GuiBuilder extends Gui
 	public void drawSlot(int x, int y, int type, boolean locked)
 	{
 		this.bind(slots);
-		this.drawSlot_(x, y, type, locked);
+		this.drawSlot_(x - 4, y - 4, type, locked);
 	}
 	
 	public void drawSlot_(int x, int y, int type, boolean locked)
 	{
-		this.drawTexturedModalRect(x, y, locked ? 32 : 0, type * 32, 32, 32);
+		this.drawTexturedModalRect(x, y, type * 32, locked ? 32 : 0, 32, 32);
 	}
 }
