23380112ee66c1d21b9724f7dc86091acffbfb8a
Clashsoft <clashsoft@hotmail.com>
2014-10-24 22:43:27 +0200

    1.7.10-2.7.2

--- src/main/java/clashsoft/cslib/minecraft/CSLib.java
+++ src/main/java/clashsoft/cslib/minecraft/CSLib.java
@@ -44,7 +44,7 @@ public class CSLib extends ClashsoftMod
 	public static final String	MODID				= "cslib";
 	public static final String	NAME				= "Clashsoft Lib";
 	public static final String	ACRONYM				= "cslib";
-	public static final String	VERSION				= "1.7.10-2.7.1";
+	public static final String	VERSION				= "1.7.10-2.7.2";
 	public static final String	DEPENDENCY			= "required-after:" + MODID;
 	
 	static
